5062ab34d5b42b8abcd4f694d44ba219f318a585
Ki Jong Byun <kjbyun0@gmail.com>
2023-06-27 20:09:02 -0500

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(thing='roller-skate') {
+    return `This Saturday, I want to ${thing}!`;
+}
+
+function mondayWork(thing = 'go to the office') {
+    return `This Monday, I will ${thing}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(detail="special") {
+      return `You are ${flair}${detail}${flair}!`;
+    };
+}
